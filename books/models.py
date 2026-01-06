7202f23b455a35e3224caa92f07a671bf6e2288b
Irene Chebet <irenetowett201@gmail.com>
2026-01-06 19:12:25 +0300

    Update models.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    available_copies = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+     
+
 # Create your models here.
