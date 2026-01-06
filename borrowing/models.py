269fe744b304ddba9d8df4e4d7e4b366236db2a8
Irene Chebet <irenetowett201@gmail.com>
2026-01-06 19:13:43 +0300

    Update models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from books.models import Book
 
-# Create your models here.
+class Borrow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    returned = models.BooleanField(default=False)
