db90d410416270be0f5cafa27b4e24f4317a5969
Irene Chebet <irenetowett201@gmail.com>
2026-01-10 16:37:17 +0300

    Update settings.py

--- library_api/settings.py
+++ library_api/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Core Django apps (needed for admin)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'books',
+    'borrowing',
 ]
 
 MIDDLEWARE = [
