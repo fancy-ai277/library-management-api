67714b7b4f39b32e7d875f2384eb40655118cddf
Irene Chebet <irenetowett201@gmail.com>
2026-01-13 22:20:41 +0300

    Update settings.py

--- library_api/settings.py
+++ library_api/settings.py
@@ -19,10 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f^f0fl1)3y*n(%5%&s$6$@lm2_)u&6i196gr@jopw!z^lu2%!8'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -43,7 +41,6 @@ INSTALLED_APPS = [
     'books',
      'borrowing',
 
-    # Third-party apps
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -126,8 +123,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
